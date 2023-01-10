e07994f7af204eed6205d73156b795e1989a8387
CharlesRado <charles.radolanirina@eleve.isep.fr>
2023-01-10 01:22:02 +0100

    sdsv

---
+++ src/BotSevenWonder.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class BotSevenWonder {
+    private GameState gameState;//état actuel du jeu qui possède une méthode 'play'
+
+    public BotSevenWonder (GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    public void play() {//méthode qui boucle jusqu'à la fin de la partie
+
+        //À chaque tour le bot récupère la liste des actions possibles et en choisit une en utilisant la méthode 'chooseBestAction' et l'applique à l'état du jeu
+        while (!gameState.isGameOver()) {
+            List<Action> possibleActions = gameState.getPossibleActions();
+            Action action = chooseBestAction(possibleActions);
+            gameState.applyAction(action);
+        }
+    }
+
+    //méthode permettant de déterminer la meilleure action possible à chaque tour
+    //ça dépend de notre stratégie et de nos objectifs pour le bot
+    private Action chooseBestAction(List<Action> actions) {
+
+    }
+
+    //Deep learning trop compliqué pour les jeux de plateau stratégique
+    //faisable avec plus de temps
+    //meilleur choix possible avec du temps là actuellement pas de temps donc infaisable
+    //maintenant ce que je peux faire c'est de faire la fonction 'chooseBestAction'
+    //fonction permettant de faire le meilleur choix possible
+    //Pour cela j'aurais besoin d'avoir l'état actuel du jeu
+    //et la liste de toutes les actions
+
+}
